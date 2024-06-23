6ccdbf1cb77fc3934dcc05ac7b6337a41b90b482
Baruch (Ben) Speiser <sbaruch@cambium.co.il>
2024-06-23 08:48:43 +0300

    Try building BoringSSL as a shared object instead, and see if that helps resolve future linking problems.

--- extension/source/third_party/boringssl/build.sh
+++ extension/source/third_party/boringssl/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 $PWD/src/util/generate_build_files.py android-cmake
+export PROJECT_ROOT=$(dirname $(readlink -e $PWD/../../..))
 cd src
 export NDK=~/android-ndk-r26d
 cmake \
@@ -8,7 +9,11 @@ cmake \
     -DANDROID_PLATFORM=android-32 \
     -GNinja \
     -B build \
-    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=1 \
+    -DCMAKE_CXX_COMPILER=$PROJECT_ROOT/libwebrtc/src/third_party/llvm-build/Release+Asserts/bin/clang
 ninja -C build
-readelf -h build/libcrypto.a | grep 'Class\|File\|Machine'
-cp build/libcrypto.a ../../../../../prebuilt/ubuntu-20.04/ndk-26.3.11579264/arm64-v8a/android-32
+readelf -h build/libcrypto.so | grep 'Class\|File\|Machine'
+cp build/libcrypto.so ../../../../../prebuilt/ubuntu-20.04/ndk-26.3.11579264/arm64-v8a/android-32
+#readelf -h build/libcrypto.a | grep 'Class\|File\|Machine'
+#cp build/libcrypto.a ../../../../../prebuilt/ubuntu-20.04/ndk-26.3.11579264/arm64-v8a/android-32
