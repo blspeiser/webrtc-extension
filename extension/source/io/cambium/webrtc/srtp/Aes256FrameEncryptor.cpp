d5c458c4de17266a4c004edcca2a5a2acaeba5a4
Baruch (Ben) Speiser <sbaruch@cambium.co.il>
2024-06-25 13:29:22 +0300

    Attempt to avoid pointer mangling

--- extension/source/io/cambium/webrtc/srtp/Aes256FrameEncryptor.cpp
+++ extension/source/io/cambium/webrtc/srtp/Aes256FrameEncryptor.cpp
@@ -65,7 +65,7 @@ JNIEXPORT jlong JNICALL Java_io_cambium_webrtc_srtp_Aes256FrameEncryptor_initial
     //otherwise
     __android_log_print(ANDROID_LOG_DEBUG, LOG_TOPIC, 
                         "Created native Aes256FrameEncryptor successfully, pointer: %p", frameEncryptor);
-    jlong pointer = reinterpret_cast<jlong>(&frameEncryptor);
+    jlong pointer = jlong(&frameEncryptor); //jlong constructor here is very important to avoid pointer mangling!!!
     return pointer;
 }
 
